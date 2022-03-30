ff09eb045948cef595ac85bfbde58a9037a4c716
harynyainarajaonah01@gmail.com <harynyainarajaonah01@gmail.com>
2022-03-30 08:04:57 +0200

    create function generate code and sendMail for confirmation

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -32,4 +32,52 @@ router.post('/login', (req, res) =>{
     });
 });
 
+router.post('/generateInscriptionCode', (req, res) =>{
+    const promise = AuthModel.generateCodeInscription(req);
+    promise.then(function(value){
+        if(value){
+            res.json({
+                status : 200, // reponse http
+                error : false, // pour signaler que ceci est une erreur
+                data : "Email est envoyé" // pour les users
+            });
+        }else{
+            res.json({
+                status : 400, // reponse http
+                error : false, // pour signaler que ceci est une erreur
+                data : "Email n'as pas pu etre envoyé" // pour les users
+            });
+        }
+    }).catch( error => {
+        console.error(error);
+        res.json({
+            status : 400, // reponse http
+            error : true, // pour signaler que ceci est une erreur
+            detailed : `${error} : concernant la requête infos `, // erreur pour les devs
+            data : "Une erreur est survenue lors de la requête" // pour les users
+        });
+    });
+});
+
+router.post('/inscription', (req, res) =>{
+    let connection = new Connection();
+	let dbpromise = connection.getDB("ekaly");
+    dbpromise.then(function(db){
+        const promise = AuthModel.inscription(db, req);
+        promise.then(function(value){
+            res.json(value);
+        }).catch( error => {
+            console.error(error);
+            res.json({
+                status : 400, // reponse http
+                error : true, // pour signaler que ceci est une erreur
+                detailed : `${error} : concernant la requête infos `, // erreur pour les devs
+                data : "Une erreur est survenue lors de la requête" // pour les users
+            });
+        }).finally(()=>{
+            connection.endConnection();
+        });
+    });
+});
+
 module.exports = router;
--- modele/AuthModel.js
+++ modele/AuthModel.js
@@ -1,4 +1,5 @@
 /* node JS */
+var EmailModel = require('../tools/Email');
 
 module.exports = class AuthModel{
 
@@ -25,23 +26,51 @@ module.exports = class AuthModel{
         });
     }
 
+    //generate code and send to mail
+    static generateCodeInscription(req){
+        return new Promise((resolve, reject)=> {
+            let code = EmailModel.genererCode(req.body.email, req.body.name);
+            let promise = EmailModel.sendCode(req.body.email, "Veuillez utiliser ce code pour continuer l'inscription", code);
+            promise.then(value =>{
+                resolve(value);
+            }).catch(error =>{
+                console.error(err);
+                reject(error);
+            });
+        });
+    }
+
     //inscription
     static inscription(db, req){
         return new Promise((resolve, reject)=> {
-            db.collection("user").insertOne(
-                req.body
-            ).toArray(function (err, result) {
-                if (err) {
-                    console.error(err);
-                    reject(error);
-					return;
-                } else {
-                    resolve({
-                        "status": 200,
-                        "data": result
-                    });
-                }
-            });
+            if(EmailModel.verifierCode(req.body.email, req.body.name, req.body.code)){
+                db.collection("user").insertOne(
+                    {
+                        name : req.body.name,
+                        email : req.body.email,
+                        password : req.body.password,
+                        type_user_id : ObjectId(req.body.type_user_id),
+                        type_user_name : req.body.type_user_name,
+                        contact : req.body.contact
+                    }
+                ).toArray(function (err, result) {
+                    if (err) {
+                        console.error(err);
+                        reject(error);
+                        return;
+                    } else {
+                        resolve({
+                            "status": 200,
+                            "data": result
+                        });
+                    }
+                });
+            }else{
+                resolve({
+                    "status": 400,
+                    "data": "vérification du code de confirmation échouer"
+                });
+            }
         });
     }
     
