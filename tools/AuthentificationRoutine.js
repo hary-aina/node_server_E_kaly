d1fbd1ee8b77f5ccef67d6a72ac773cf8c894571
harynyainarajaonah01@gmail.com <harynyainarajaonah01@gmail.com>
2022-04-08 18:53:01 +0200

    dashboard ekaly

--- tools/AuthentificationRoutine.js
+++ tools/AuthentificationRoutine.js
@@ -4,44 +4,47 @@ const AuthentificationRoutine = {};
 
 AuthentificationRoutine.check = (req, res, next) => {
 
-	next();
+	//next();
 
 	// url sans protection
-	// let exceptions = [
-	// 	'/login', '/inscription',
-	// 	'/generercode', '/client/plat/listePlat',
-	//  '/client/plat/voirPlat', '/client/plat/searchPlat'
-	// ];
-	// let filtre = exceptions.filter(url => req.url.startsWith(url));
-	// if ( filtre.length > 0 ) {
-	// 	next();
-	// } else {		
-	// 	let token = '';
-	// 	if ( req.method == 'GET' ) {
-	// 		token = req.query.token; // le token doit figurer sur l'url 
-	// 	} else if ( req.method == 'POST' ) {
-	// 		token = req.body.token; // le token doit figurer dans le corps de la reponse
-	// 	} else if ( req.params['token'] != undefined) {
-	// 		token = req.params.token;
-	// 	}
-	// 	if ( token == '' || token == undefined || token == null ) {
-	// 		// si toute verification echoue, on verifie le header en dernier recours
-	// 		token = req.headers['token'] || '';
-	// 	}
+	let exceptions = [
+		'/login', '/inscription',
+		'/generercode', '/client/plat/listePlat',
+	 	'/client/plat/voirPlat', '/client/plat/searchPlat',
+		'/client/login', '/resto/login', '/responsable/login',
+		'/livreur/login', '/client/generateInscriptionCode', 
+		'/client/inscription'
+	];
+	let filtre = exceptions.filter(url => req.url.startsWith(url));
+	if ( filtre.length > 0 ) {
+		next();
+	} else {		
+		let token = '';
+		if ( req.method == 'GET' ) {
+			token = req.query.token; // le token doit figurer sur l'url 
+		} else if ( req.method == 'POST' ) {
+			token = req.body.token; // le token doit figurer dans le corps de la reponse
+		} else if ( req.params['token'] != undefined) {
+			token = req.params.token;
+		}
+		if ( token == '' || token == undefined || token == null ) {
+			// si toute verification echoue, on verifie le header en dernier recours
+			token = req.headers['token'] || '';
+		}
 		
-	// 	const onSuccess = () => {
-	// 		next(); // on poursuit
-	// 	};
-	// 	const onError = err => {
-	// 		res.json({
-	// 			code : 400,
-	// 			error : true,
-	// 			detailed : err,
-	// 			data : 'Session invalide, manquante ou expirée'
-	// 		});
-	// 	};
-	// 	TokenManager.verify(token, onSuccess, onError);	
-	// }
+		const onSuccess = () => {
+			next(); // on poursuit
+		};
+		const onError = err => {
+			res.json({
+				code : 400,
+				error : true,
+				detailed : err,
+				data : 'Session invalide, manquante ou expirée'
+			});
+		};
+		TokenManager.verify(token, onSuccess, onError);	
+	}
 }
 
 module.exports = AuthentificationRoutine;
